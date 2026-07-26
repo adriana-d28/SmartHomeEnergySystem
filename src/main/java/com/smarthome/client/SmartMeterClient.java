4331aa1b26b52d3464772eab40a426b5c83e3d71
Adriana Dinelly <adrianasdinelly@gmail.com>
2026-07-26 17:08:50 +0100

    Implemented gRPC Smart Meter Service server (client and server streamings) and a simple test client

--- src/main/java/com/smarthome/client/SmartMeterClient.java
+++ src/main/java/com/smarthome/client/SmartMeterClient.java
@@ -4,11 +4,58 @@
  */
 package com.smarthome.client;
 
+import com.smarthome.smartmeter.GenerateEnergyReportRequest;
+import com.smarthome.smartmeter.ReportEntry;
+import com.smarthome.smartmeter.SmartMeterServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.Iterator;
+
 /**
  *
- * @author Adriana
- * This class is used to communicate with the other services
+ * @author Adriana Dinelly - ID 25165771
+ * 
+ * This class is used to communicate with the other services. Simple Smart Meter Client for tests
+ * 
  */
 public class SmartMeterClient {
-    
+    // Server address.
+    private static final String HOST = "localhost";
+
+    // Server port.
+    private static final int PORT = 50053;
+
+    // Main Method
+    public static void main(String[] args) {
+
+        // Create the communication channel.
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(HOST, PORT).usePlaintext().build();
+        
+        // Creates a blocking stub.
+        SmartMeterServiceGrpc.SmartMeterServiceBlockingStub stub = SmartMeterServiceGrpc.newBlockingStub(channel);
+
+        // Creates the request message.
+        GenerateEnergyReportRequest request = GenerateEnergyReportRequest.newBuilder().setHouseId("HOUSE001").build();
+
+        // We use an Iterator to get all the server responses of the Server Streaming RPC.
+        Iterator<ReportEntry> reportEntries = stub.generateEnergyReport(request);
+
+        System.out.println("===== ENERGY REPORT =====");
+
+        // Reads every report entry received from the server.
+        while (reportEntries.hasNext()) {
+
+            // Retrieves the next report entry.
+            ReportEntry report = reportEntries.next();
+
+            System.out.println("--------------------------------");
+            System.out.println("Section: " + report.getSection());
+            System.out.println("Value: " + report.getValue());
+            System.out.println("Description: " + report.getDescription());
+        }
+        // Closes the communication channel.
+        channel.shutdown();
+    }     
 }
--- src/main/java/com/smarthome/server/smartmeter/SmartMeterServer.java
+++ src/main/java/com/smarthome/server/smartmeter/SmartMeterServer.java
@@ -4,10 +4,47 @@
  */
 package com.smarthome.server.smartmeter;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 /**
  *
- * @author Adriana
+ * @author Adriana Dinelly - ID 25165771
+ * 
+ * This class starts the Smart Meter gRPC server and keeps it listening for clients.
+ * 
  */
 public class SmartMeterServer {
-    
+    // Port where the Battery Service will listen for client requests.
+    private static final int PORT = 50053;
+
+    // This object represents a gRPC server instance.
+    private Server server;
+
+    // We create a server instance, start it and keep it running by calling the methods
+    public static void main(String[] args) throws IOException, InterruptedException {
+        
+        SmartMeterServer server = new SmartMeterServer();
+        server.start();
+        server.server.awaitTermination();
+    }
+
+    // This small method creates and starts the gRPC server.
+    public void start() throws IOException {
+        
+        server = ServerBuilder.forPort(PORT)
+                // Registers the service implementation.
+                .addService(new SmartMeterServiceImpl())
+                // Builds the server.
+                .build()
+                // Starts listening for client connections.
+                .start();
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Smart Meter Service started.");
+        System.out.println("Listening on port " + PORT);
+        System.out.println("------------------------------------------------");
+    }
 }
